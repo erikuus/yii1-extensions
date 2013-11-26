a2af6b9dc402cb6aecfe3d941f12d3ef80671ccd
Erik Uus <erik.uus@gmail.com>
2013-11-26 11:55:48 +0200

    Added CJuiDatePicker skin

---
+++ skins/silver/CJuiDatePicker.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * To apply classic skin, assign skin attribute:
+ * $this->widget('CJuiDatePicker', array('skin'=>'notDefault'));
+ */
+return array(
+	'default'=>array(
+		'options'=>array(
+			'dateFormat'=>'dd.mm.yy',
+		),
+	),
+);
