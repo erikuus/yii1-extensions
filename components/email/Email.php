cc97c62fb8bd508a4bb19a505ee8c47d42f01e98
Erik Uus <erik.uus@gmail.com>
2015-04-09 02:08:02 +0300

    Email bugfix

--- components/email/Email.php
+++ components/email/Email.php
@@ -186,7 +186,7 @@ class Email extends CApplicationComponent {
 			case 'php':
 				$message = wordwrap($message, $this->lineLength);
 				mb_language($this->language);
-				return mb_send_mail($to, $subject, $message, implode("\r\n", $this->createHeaders()), $additionalParams);
+				return mb_send_mail($to, $subject, $message, implode("\r\n", $this->createHeaders()), $this->additionalParams);
 			case 'debug':
 				$debug = Yii::app()->controller->renderPartial('email.debug', array_merge(
 					compact('to', 'subject', 'message'),
