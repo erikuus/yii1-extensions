4e8ad8c296fc969d2911b0957dbae9119bdb9a1f
Erik Uus <erik.uus@gmail.com>
2017-12-01 15:34:20 +0200

    Facebook tweaks

--- components/eauth/custom_services/CustomFacebookOAuthService.php
+++ components/eauth/custom_services/CustomFacebookOAuthService.php
@@ -10,21 +10,30 @@ require_once dirname(dirname(__FILE__)).'/services/FacebookOAuthService.php';
 
 class CustomFacebookOAuthService extends FacebookOAuthService
 {
+	protected $scope = 'email user_birthday';
+
+	protected $providerOptions = array(
+		'authorize' => 'https://www.facebook.com/v2.8/dialog/oauth',
+		'access_token' => 'https://graph.facebook.com/oauth/access_token',
+	);
+
+	protected $attributeNames = array(
+		'name',
+		'email',
+		'birthday',
+	);
+
 	protected function fetchAttributes()
 	{
-		$info = (object)$this->makeSignedRequest('https://graph.facebook.com/me');
+		$info = (object)$this->makeSignedRequest('https://graph.facebook.com/me', array(
+			'query' => array(
+				'fields'=>implode(',', $this->attributeNames)
+			)
+		));
 
 		$this->attributes['id']= $info->id;
-
-		if (isset($info->name))
-			$this->attributes['name'] = $info->name;
-
-		if (isset($info->url))
-			$this->attributes['url'] = $info->link;
-
-		if (isset($info->email))
-			$this->attributes['email'] = $info->email;
-
-		$this->attributes['info'] = $info;
+		$this->attributes['name'] = isset($info->name) ? $info->name : null;
+		$this->attributes['email'] = isset($info->email) ? $info->email : null;
+		$this->attributes['birthday'] = isset($info->birthday) ? $info->birthday : null;
 	}
 }
