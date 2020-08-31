52900c0da65fef58a3060d9262945e2d64995808
Erik Uus <erik.uus@gmail.com>
2020-08-31 19:37:12 +0300

    Fix example in docblock

--- widgets/bookreader/XBookReader.php
+++ widgets/bookreader/XBookReader.php
@@ -16,7 +16,7 @@
  *     </style>
  * </head>
  * <body>
- * $this->widget('ext.widgets.bookreader.XBookReader', array(
+ * <?php $this->widget('ext.widgets.bookreader.XBookReader', array(
  *    'enableMobileNav'=>true,
  *    'enablePluginUrl'=>true,
  * 	  'htmlOptions'=>array(
@@ -73,7 +73,7 @@
  *            )
  *        )
  *    )
- * ));
+ * )); ?>
  * </body>
  * </html>
  *
