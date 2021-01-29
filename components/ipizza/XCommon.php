fd45a624cf10c24c689df8f710c4e33f1819bb1b
Erik Uus <erik.uus@gmail.com>
2021-01-29 11:18:27 +0200

    Asendab tühikud tabulaatoriga

--- components/ipizza/XCommon.php
+++ components/ipizza/XCommon.php
@@ -73,97 +73,97 @@ class XCommon extends XIPizza
 		);
 	}
 
-    /**
-     * @return array param length
-     */
-    protected function getParamLengths()
-    {
-        return array(
-            'VK_SERVICE' => 4,
-            'VK_VERSION' => 3,
-            'VK_SND_ID' => 15,
-            'VK_STAMP' => 20,
-            'VK_AMOUNT' => 12,
-            'VK_CURR' => 3,
-            'VK_ACC' => 34,
-            'VK_NAME' => 70,
-            'VK_REF' => 35,
-            'VK_MSG' => 95,
-            'VK_RETURN' => 255,
-            'VK_CANCEL' => 255,
-        	'VK_DATETIME' => 24,
-            'VK_MAC' => 700,
-            'VK_ENCODING' => 12,
-            'VK_LANG' => 3,
-        );
-    }
+	/**
+	 * @return array param length
+	 */
+	protected function getParamLengths()
+	{
+		return array(
+			'VK_SERVICE' => 4,
+			'VK_VERSION' => 3,
+			'VK_SND_ID' => 15,
+			'VK_STAMP' => 20,
+			'VK_AMOUNT' => 12,
+			'VK_CURR' => 3,
+			'VK_ACC' => 34,
+			'VK_NAME' => 70,
+			'VK_REF' => 35,
+			'VK_MSG' => 95,
+			'VK_RETURN' => 255,
+			'VK_CANCEL' => 255,
+			'VK_DATETIME' => 24,
+			'VK_MAC' => 700,
+			'VK_ENCODING' => 12,
+			'VK_LANG' => 3,
+		);
+	}
 
 	/**
 	 * @return array mac params names mapped by service id
 	 */
-    protected function getMacParamMap()
-    {
-        return array(
-            // request to make a transaction
-            '1011' => array(
-                'VK_SERVICE',
-                'VK_VERSION',
-                'VK_SND_ID',
-                'VK_STAMP',
-                'VK_AMOUNT',
-                'VK_CURR',
-                'VK_ACC',
-                'VK_NAME',
-                'VK_REF',
-                'VK_MSG',
-            	'VK_RETURN',
-            	'VK_CANCEL',
-            	'VK_DATETIME'
-            ),
-            // request to make a transaction
-            '1012' => array(
-                'VK_SERVICE',
-                'VK_VERSION',
-                'VK_SND_ID',
-                'VK_STAMP',
-                'VK_AMOUNT',
-                'VK_CURR',
-                'VK_REF',
-                'VK_MSG',
-            	'VK_RETURN',
-            	'VK_CANCEL',
-            	'VK_DATETIME'
-            ),
-            // 'transaction completed' response message
-            '1111' => array(
-                'VK_SERVICE',
-                'VK_VERSION',
-                'VK_SND_ID',
-                'VK_REC_ID',
-                'VK_STAMP',
-                'VK_T_NO',
-                'VK_AMOUNT',
-                'VK_CURR',
-                'VK_REC_ACC',
-                'VK_REC_NAME',
-                'VK_SND_ACC',
-                'VK_SND_NAME',
-                'VK_REF',
-                'VK_MSG',
-                'VK_T_DATETIME'
-            ),
-            // 'transaction not completed' response message
-            '1911' => array(
-                'VK_SERVICE',
-                'VK_VERSION',
-                'VK_SND_ID',
-                'VK_REC_ID',
-                'VK_STAMP',
-                'VK_REF',
-                'VK_MSG'
-            ),
-        );
-    }
+	protected function getMacParamMap()
+	{
+		return array(
+			// request to make a transaction
+			'1011' => array(
+				'VK_SERVICE',
+				'VK_VERSION',
+				'VK_SND_ID',
+				'VK_STAMP',
+				'VK_AMOUNT',
+				'VK_CURR',
+				'VK_ACC',
+				'VK_NAME',
+				'VK_REF',
+				'VK_MSG',
+				'VK_RETURN',
+				'VK_CANCEL',
+				'VK_DATETIME'
+			),
+			// request to make a transaction
+			'1012' => array(
+				'VK_SERVICE',
+				'VK_VERSION',
+				'VK_SND_ID',
+				'VK_STAMP',
+				'VK_AMOUNT',
+				'VK_CURR',
+				'VK_REF',
+				'VK_MSG',
+				'VK_RETURN',
+				'VK_CANCEL',
+				'VK_DATETIME'
+			),
+			// 'transaction completed' response message
+			'1111' => array(
+				'VK_SERVICE',
+				'VK_VERSION',
+				'VK_SND_ID',
+				'VK_REC_ID',
+				'VK_STAMP',
+				'VK_T_NO',
+				'VK_AMOUNT',
+				'VK_CURR',
+				'VK_REC_ACC',
+				'VK_REC_NAME',
+				'VK_SND_ACC',
+				'VK_SND_NAME',
+				'VK_REF',
+				'VK_MSG',
+				'VK_T_DATETIME'
+			),
+			// 'transaction not completed' response message
+			'1911' => array(
+				'VK_SERVICE',
+				'VK_VERSION',
+				'VK_SND_ID',
+				'VK_REC_ID',
+				'VK_STAMP',
+				'VK_REF',
+				'VK_MSG'
+			),
+		);
+	}
 
 	/**
 	 * Check whether it is automated request
