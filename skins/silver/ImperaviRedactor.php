e5df7e1106e9eee55d4950714fec9b1c38515afe
Erik Uus <erik.uus@gmail.com>
2021-03-29 10:34:27 +0300

    Lisab imperavi redaktori vaikimisi seaded

---
+++ skins/silver/ImperaviRedactor.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Global defination for ImperaviRedactor
+ */
+return array(
+	'default'=>array(
+		'package'=>array(
+			'js'=>array(
+				YII_DEBUG ? 'jquery-1.8.3.js' : 'jquery-1.8.3.min.js',
+				YII_DEBUG ? 'redactor.js' : 'redactor.min.js',
+			)
+		)
+	)
+);
