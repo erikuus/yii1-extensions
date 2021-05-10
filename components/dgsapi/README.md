9099021af3dde53947d3d8090474254bf3665355
Erik Uus <erik.uus@gmail.com>
2021-05-10 13:19:38 +0300

    Add dgsapi extension

---
+++ components/dgsapi/README.md
@@ -0,0 +1,31 @@
+Yii 1 raamistiku laiendus DGS API kasutamiseks
+==========================================================
+
+https://github.com/erikuus/yii1-extensions/tree/master/components/dgsapi
+https://www.ra.ee/dgs-api/doc/index
+
+Paigaldus
+-----------------------
+Laadi laienduse failid Githubist alla ja salvesta need **extensions/components/dgsapi/** kausta.
+
+Seadistus
+--------------------
+Lisa konfiguratsioonifailis komponentide hulka **XDgsApi**:
+
+```php
+'components'=>array(
+    'dgs'=> array(
+        'class'=>'ext.components.dgsapi.XDgsApi',
+        'url'=>'https://www.ra.ee/dgs-api/api/v1/dip/'
+    )
+)
+```
+
+Kasutamine
+--------------------
+
+```php
+Yii::app()->dgs->request('get-file-path', array(
+	'uid'=>'b1bc1876-d34b-57d1-a7a5-7563ef53333b'
+);
+```
---
+++ components/dgsapi/XDgsApi.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * XDgsApi component enables to call DGS API
+ *
+ * @link https://www.ra.ee/dgs-api/doc/index
+ * @author Erik Uus <erik.uus@gmail.com>
+ * @version 1.0.0
+ */
+
+class XDgsApi extends CApplicationComponent
+{
+	/**
+	 * @var string the url where API calls are sent to
+	 */
+	public $url;
+
+	/**
+	 * Send API request
+	 * @param string $request the name of the request
+	 * @param array $params parameters (name=>value) of the request
+	 * @return array response
+	 */
+	public function request($request, $parameters=array())
+	{
+		// create request
+		$handle=curl_init($this->url.$request);
+
+		// set the payload
+		curl_setopt($handle, CURLOPT_POST, true);
+		curl_setopt($handle, CURLOPT_POSTFIELDS, $parameters);
+
+		// return body only
+		curl_setopt($handle, CURLOPT_HEADER, 0);
+		curl_setopt($handle, CURLOPT_RETURNTRANSFER, 1);
+
+		// create errors on timeout and on response code >= 300
+		curl_setopt($handle, CURLOPT_TIMEOUT, 45);
+		curl_setopt($handle, CURLOPT_FAILONERROR, true);
+		curl_setopt($handle, CURLOPT_FOLLOWLOCATION, false);
+
+		// set up verification
+		curl_setopt($handle, CURLOPT_SSL_VERIFYHOST, 2);
+		curl_setopt($handle, CURLOPT_SSL_VERIFYPEER, false);
+
+		// run
+		$response=curl_exec($handle);
+		$error=curl_error($handle);
+		$errorNumber=curl_errno($handle);
+		curl_close($handle);
+
+		if($error)
+			throw new Exception('CURL error: '.$response.':'.$error.': '.$errorNumber);
+
+		return json_decode($response, true);
+	}
+}
