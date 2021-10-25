0d7f0baa62556c1b4842403e0bc98e174b223120
Erik Uus <erik.uus@gmail.com>
2021-10-26 00:28:00 +0300

    Update XWebService.php

--- components/webservice/XWebService.php
+++ components/webservice/XWebService.php
@@ -2,20 +2,15 @@
 /**
  * XWebService class file.
  *
- * XWebService extends CWebService generating wsdl without any cache
+ * XWebService extends CWebService generating wsdl from file
  *
  * @author Erik Uus <erik.uus@gmail.com>
  * @version 1.0.0
  */
 class XWebService extends CWebService
 {
-	public $generatorConfig='CWsdlGenerator';
-		
 	public function generateWsdl()
 	{
-		$providerClass=is_object($this->provider) ? get_class($this->provider) : Yii::import($this->provider,true);
-		$generator=Yii::createComponent($this->generatorConfig);
-		$wsdl=$generator->generateWsdl($providerClass,$this->serviceUrl,$this->encoding);
-		return $wsdl;
+		return file_get_contents($this->wsdlUrl);
 	}
 }
