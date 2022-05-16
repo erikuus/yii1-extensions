0121f45e4f79035ee7e5af4baf4e43cee0d683bc
Erik Uus <erik.uus@gmail.com>
2022-05-16 13:14:05 +0300

    Add html helper messages

--- helpers/XHtml.php
+++ helpers/XHtml.php
@@ -140,13 +140,13 @@ class XHtml extends CHtml
 		$min=floor(($sec-($days*60*60*24)-($hours*60*60))/60);
 		$sec=floor($sec-($days*60*60*24+$hours*60*60+$min*60));
 
-		$human=Yii::t('ui','n==0#|n==1#1 day |n>1#{n} days ', array($days)).Yii::t('ui','n==0#|n==1#1 hour |n>1#{n} hours ', array($hours));
+		$human=Yii::t('XHtml.html','n==0#|n==1#1 day |n>1#{n} days ', array($days)).Yii::t('XHtml.html','n==0#|n==1#1 hour |n>1#{n} hours ', array($hours));
 
 		if($days==0 || $hours==0)
-			$human.=Yii::t('ui','n==0#|n==1#1 minute |n>1#{n} minutes ', array($min));
+			$human.=Yii::t('XHtml.html','n==0#|n==1#1 minute |n>1#{n} minutes ', array($min));
 
 		if($days==0 && $hours==0)
-			$human.=Yii::t('ui','n==0#|n==1#1 second|n>1#{n} seconds', array($sec));
+			$human.=Yii::t('XHtml.html','n==0#|n==1#1 second|n>1#{n} seconds', array($sec));
 
 		return $human;
 	}
---
+++ helpers/messages/et/html.php
@@ -0,0 +1,7 @@
+<?php
+return array (
+  'n==0#|n==1#1 day |n>1#{n} days ' => 'n==0#|n==1#1 päev |n>1#{n} päeva ',
+  'n==0#|n==1#1 hour |n>1#{n} hours ' => 'n==0#|n==1#1 tund |n>1#{n} tundi ',
+  'n==0#|n==1#1 minute |n>1#{n} minutes ' => 'n==0#|n==1#1 minut |n>1#{n} minutit ',
+  'n==0#|n==1#1 second|n>1#{n} seconds' => 'n==0#|n==1#1 sekund|n>1#{n} sekundit '
+);
