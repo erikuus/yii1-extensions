ceaa7b0c0f752c785f42a590bc7c52dc41840770
Erik Uus <erik.uus@gmail.com>
2022-12-29 19:55:07 +0200

    Update XPinal.php

--- components/pinal/XPinal.php
+++ components/pinal/XPinal.php
@@ -194,7 +194,7 @@ class XPinal extends CApplicationComponent
 	 * @param string $importXml the xml content that defines what and where to create or update.
 	 *
 	 * For example, document can be created as follows:
-	 * <?xml version="1.0" encoding="utf-8"?>
+	 *
 	 * <system schemaVersion="1" xmlns="http://www.nortal.com/FlairPoint/RecordsManagement/WebCapture">
 	 *     <hierarchy>
 	 *         <parent>
@@ -214,7 +214,7 @@ class XPinal extends CApplicationComponent
 	 * </system>
 	 *
 	 * And later this document can be updated as follows:
-	 * <?xml version="1.0" encoding="utf-8"?>
+	 *
 	 * <system schemaVersion="1" xmlns="http://www.nortal.com/FlairPoint/RecordsManagement/WebCapture">
 	 *     <hierarchy>
 	 *         <parent>
