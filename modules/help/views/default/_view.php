70b23fb7681184cca18da5eab61a4c3a2e504efe
Erik Uus <erik.uus@gmail.com>
2023-12-05 21:31:15 +0200

    Update _view.php

--- modules/help/views/default/_view.php
+++ modules/help/views/default/_view.php
@@ -1,4 +1,4 @@
-<h2 class="font-heading text-lg font-bold text-gray-700 py-6">
+<h2 class="font-heading text-lg font-bold text-gray-700 my-4">
 	<?php echo $model->{'title_'.Yii::app()->language}; ?>
 	<?php if($this->module->editOnPage): ?>
 		<?php echo $this->getEditLink($model->id); ?>
