162aeddc58a2305e599cc53ab9027ef89e9a47f3
Erik Uus <erik.uus@gmail.com>
2023-12-15 12:12:02 +0200

    Update XTHtml.php

--- components/tailwind/XTHtml.php
+++ components/tailwind/XTHtml.php
@@ -10,42 +10,24 @@
  */
 class XTHtml extends CHtml
 {
-	/**
-	 * @var string the CSS class for displaying error summaries (see {@link errorSummary}).
-	 */
+
 	public static $errorSummaryCss='-mx-4 my-4 rounded bg-red-50 p-4 text-sm text-red-700';
+
 	public static $errorSummaryHeaderCss='mb-4 font-medium';
+
 	public static $errorSummaryListCss='ml-6 list-disc space-y-2';
-	/**
-	 * @var string the CSS class for highlighting error inputs. Form inputs will be appended
-	 * with this CSS class if they have input errors.
-	 */
+
 	public static $errorCss='border-red-300 hover:border-red-400 focus:border-red-600 focus:ring-red-200 focus:ring-red-200';
-	/**
-	 * @var string the CSS class for displaying link.
-	 */
+
 	public static $linkCssClass='font-medium text-blue-600 hover:text-blue-500 focus:active:text-blue-700';
-	/**
-	 * @var string the CSS class for displaying link.
-	 */
+
 	public static $buttonCssClass='focus-visible::ring-2 focus-visible::ring-green-300 cursor-pointer rounded bg-gray-900 py-3 pl-3 pr-4 text-center text-base font-medium text-white hover:bg-gray-700';
-	/**
-	 * @var string the CSS class name for labels.
-	 * Individual {@link error} call may override this value by specifying the 'class' HTML option.
-	 */
+
+	public static $buttonLinkCssClass='focus-visible::ring-2 focus-visible::ring-green-300 cursor-pointer border border-gray-200 rounded bg-white py-3 pl-3 pr-4 text-center text-base font-medium text-gray-900 hover:bg-gray-100';
+
 	public static $labelCssClass='mb-2 block text-base font-semibold leading-6 text-gray-800';
 
-	/**
-	 * Generates a hyperlink tag.
-	 * @param string $text link body. It will NOT be HTML-encoded. Therefore you can pass in HTML code such as an image tag.
-	 * @param mixed $url a URL or an action route that can be used to create a URL.
-	 * See {@link normalizeUrl} for more details about how to specify this parameter.
-	 * @param array $htmlOptions additional HTML attributes. Besides normal HTML attributes, a few special
-	 * attributes are also recognized (see {@link clientChange} and {@link tag} for more details.)
-	 * @return string the generated hyperlink
-	 * @see normalizeUrl
-	 * @see clientChange
-	 */
+
 	public static function link($text,$url='#',$htmlOptions=array())
 	{
 		$htmlOptions=self::assignClass($htmlOptions, self::$linkCssClass);
@@ -56,14 +38,16 @@ class XTHtml extends CHtml
 		return self::tag('a',$htmlOptions,$text);
 	}
 
-	/**
-	 * Generates a submit button.
-	 * @param string $label the button label
-	 * @param array $htmlOptions additional HTML attributes. Besides normal HTML attributes, a few special
-	 * attributes are also recognized (see {@link clientChange} and {@link tag} for more details.)
-	 * @return string the generated button tag
-	 * @see clientChange
-	 */
+	public static function buttonLink($text,$url='#',$htmlOptions=array())
+	{
+		$htmlOptions=self::assignClass($htmlOptions, self::$buttonLinkCssClass);
+
+		if($url!=='')
+			$htmlOptions['href']=self::normalizeUrl($url);
+		self::clientChange('click',$htmlOptions);
+		return self::tag('a',$htmlOptions,$text);
+	}
+
 	public static function submitButton($label='submit',$htmlOptions=array())
 	{
 		$htmlOptions=self::assignClass($htmlOptions, self::$buttonCssClass);
@@ -72,26 +56,6 @@ class XTHtml extends CHtml
 		return self::button($label,$htmlOptions);
 	}
 
-	/**
-	 * Generates a label tag for a model attribute.
-	 * The label text is the attribute label and the label is associated with
-	 * the input for the attribute (see {@link CModel::getAttributeLabel}.
-	 * If the attribute has input error, the label's CSS class will be appended with {@link errorCss}.
-	 * @param CModel $model the data model
-	 * @param string $attribute the attribute
-	 * @param array $htmlOptions additional HTML attributes. The following special options are recognized:
-	 * <ul>
-	 * <li>required: if this is set and is true, the label will be styled
-	 * with CSS class 'required' (customizable with CHtml::$requiredCss),
-	 * and be decorated with {@link CHtml::beforeRequiredLabel} and
-	 * {@link CHtml::afterRequiredLabel}. This option has been available since version 1.0.2.</li>
-	 * <li>label: this specifies the label to be displayed. If this is not set,
-	 * {@link CModel::getAttributeLabel} will be called to get the label for display.
-	 * If the label is specified as false, no label will be rendered.
-	 * This option has been available since version 1.0.4.</li>
-	 * </ul>
-	 * @return string the generated label tag
-	 */
 	public static function activeLabel($model,$attribute,$htmlOptions=array())
 	{
 		$htmlOptions=self::assignClass($htmlOptions, self::$labelCssClass);
@@ -116,22 +80,6 @@ class XTHtml extends CHtml
 		return self::label($label,$for,$htmlOptions);
 	}
 
-	/**
-	 * Displays a summary of validation errors for one or several models.
-	 * @param mixed $model the models whose input errors are to be displayed. This can be either
-	 * a single model or an array of models.
-	 * @param string $header a piece of HTML code that appears in front of the errors
-	 * @param string $footer a piece of HTML code that appears at the end of the errors
-	 * @param array $htmlOptions additional HTML attributes to be rendered in the container div tag.
-	 * This parameter has been available since version 1.0.7.
-	 * A special option named 'firstError' is recognized, which when set true, will
-	 * make the error summary to show only the first error message of each attribute.
-	 * If this is not set or is false, all error messages will be displayed.
-	 * This option has been available since version 1.1.3.
-	 * @return string the error summary. Empty if no errors are found.
-	 * @see CModel::getErrors
-	 * @see errorSummaryCss
-	 */
 	public static function errorSummary($model,$header=null,$footer=null,$htmlOptions=array())
 	{
 		$content='';
