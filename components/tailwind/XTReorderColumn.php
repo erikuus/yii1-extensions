dbb5baa8a8a4bee65ba6d0fa62f017508cf86d8a
Erik Uus <erik.uus@gmail.com>
2024-10-24 17:21:06 +0300

    Update XTReorderColumn.php

--- components/tailwind/XTReorderColumn.php
+++ components/tailwind/XTReorderColumn.php
@@ -22,20 +22,23 @@ class XTReorderColumn extends CGridColumn
 	 * @var string css class name for reorder links. Defaults to 'reorder'
 	 * Note that you have to define different class names when using multiple gridviews per one page.
 	 */
-	public $cssClass='reorder';
+	public $triggerCssClass='reorder';
+	/**
+	 * @var string button css class
+	 */
+	public $buttonCssClass='inline-flex h-8 w-8 flex-shrink-0 items-center justify-center rounded-full text-gray-500 hover:bg-green-100 hover:text-gray-800';	
 	/**
 	 * @var string the up icon.
 	 */
-	public $upIcon='<svg class="w-6 h-6" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path d="M4 12L5.41 13.41L11 7.83V20H13V7.83L18.58 13.42L20 12L12 4L4 12Z" fill="currentColor"></path></svg>';
+	public $upIcon='<svg class="w-5 h-5" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path d="M4 12L5.41 13.41L11 7.83V20H13V7.83L18.58 13.42L20 12L12 4L4 12Z" fill="currentColor"></path></svg>';
 	/**
 	 * @var string the down icon.
 	 */
-	public $downIcon='<svg class="w-6 h-6" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path d="M20 12L18.59 10.59L13 16.17V4H11V16.17L5.42 10.58L4 12L12 20L20 12Z" fill="currentColor"></path></svg>';
+	public $downIcon='<svg class="w-5 h-5" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path d="M20 12L18.59 10.59L13 16.17V4H11V16.17L5.42 10.58L4 12L12 20L20 12Z" fill="currentColor"></path></svg>';
 	/**
 	 * @var array the HTML options for the data cell tags.
-	 * Defaults to array('class'=>'button-column')
 	 */
-	public $htmlOptions=array('class'=>'whitespace-nowrap');
+	public $htmlOptions=array();
 	/**
 	 * @var mixed boolean or PHP expression that is evaluated for every data cell whether the reorder links are visible. Defaults to true.
 	 */
@@ -60,7 +63,7 @@ class XTReorderColumn extends CGridColumn
 	{
 		$gridId = $this->grid->getId();
 		$script = <<<SCRIPT
-		jQuery(".{$this->cssClass}").live("click", function(e){
+		jQuery(".{$this->triggerCssClass}").live("click", function(e){
 			e.preventDefault();
 			$.fn.yiiGridView.update("$gridId", {
 				type:"POST",
@@ -116,7 +119,7 @@ SCRIPT;
 		$url=CHtml::normalizeUrl($this->callbackUrl);
 
 		echo CHtml::link($icon, $url, array(
-			'class'=>$this->cssClass
+			'class'=>$this->triggerCssClass.' '.$this->buttonCssClass
 		));
 	}
 }
