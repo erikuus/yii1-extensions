07243ef80a1ed9096b6848a4cd056be3cdf409d4
Erik Uus <erik.uus@gmail.com>
2025-02-12 12:28:16 +0200

    Create XLeafletInputMap.php

---
+++ widgets/leaflet/messages/et/XLeafletInputMap.php
@@ -0,0 +1,5 @@
+<?php
+return array (
+  'Clear map' => 'Puhasta kaart',
+  'Click the rectangle button to draw a box. Click the edit button to move or resize it.' => 'Klõpsa ristküliku nuppu, et joonistada kast. Klõpsa redigeerimisnuppu, et seda liigutada või muuta.',
+);
